9afe819cadb14d16706ad8609d7949b877aef08d
Bruce Ren <renliangfeng2011@gmail.com>
2023-09-12 09:30:10 +1000

    1.2 release notes and update document

--- mvn-install/install-sail4j-jars.sh
+++ mvn-install/install-sail4j-jars.sh
@@ -3,10 +3,10 @@
 # These plugins assist in the build of an IdentityIQ implementation project.
 
 # Original Author: Bruce Ren (bruce.ren@sailpoint.com)
-# Version: 1.1
+# Version: 1.2
 # Date: 25-May-2023
 
-export VERSION=1.1
+export VERSION=1.2
 
 mvn install:install-file -DgroupId=com.sailpoint.sail4j -DartifactId=sail4j -Dversion=${VERSION} -Dpackaging=pom -Dfile=../sail4j-bundle/sail4j-${VERSION}.pom
 mvn org.apache.maven.plugins:maven-install-plugin:3.0.0-M1:install-file -Dfile=../sail4j-bundle/sail4j-api-${VERSION}.jar
